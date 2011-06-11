bcf605be797003ed4ed55d4ea194fc26995af518
Ryland Herrick <ryland@ry-land.com>
2011-06-11 16:19:37 -0500

    Initial Commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jungola::Application.config.secret_token = '3f1a37ca4ccefad1faece98386cc137c64a73e0988a07600f49d892b0dce84d1d5d886b3d0eed222a1f6b82171d6bc6c750825c98568fbd94e1a0f591897629e'
