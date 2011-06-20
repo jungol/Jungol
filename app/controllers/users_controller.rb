657fc4ccf0c17ff53542e6ee5113d09d08bf4d33
Ryland Herrick <ryland@ry-land.com>
2011-06-20 16:01:05 -0500

    Added signup functionality.
    Added a signup form, validation and corresponding error messages.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @title = @user.name
   end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to Jungol!"
+      redirect_to @user
+    else
+      @title = "Sign up"
+      render :new
+    end
+  end
 end
