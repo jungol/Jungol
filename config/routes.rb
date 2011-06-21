0b6858bc45af662c35c51250b6a1e6e3609dda14
Ryland Herrick <ryland@ry-land.com>
2011-06-20 22:35:32 -0500

    Added signin functionality
    Added session controller to deal with cookies related to signin.
    Also added a footer for appearance's sake.  Signout and integration
    tests to come.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Jungola::Application.routes.draw do
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
 
   match '/signup', :to => 'users#new'
+  match '/signin', :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
 
   root :to => 'pages#home'
 
