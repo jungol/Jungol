9714b24208261b6dbea8474bf33fd5e3451ea029
Ryland Herrick <ryland@ry-land.com>
2011-07-23 00:38:10 -0500

    Added database check to cucumber tests.
    Mostly because Item sharing wasn't tested elsewhere.

--- features/step_definitions/group_steps.rb
+++ features/step_definitions/group_steps.rb
@@ -1,22 +1,28 @@
 When /^"([^""]*)" shares a Todo with "([^""]*)"$/ do |group1, group2|
   Given "\"#{group1}\" has a Todo"
-    And "I am on #{group1}'s page"
-    And "I follow \"Something\""
+  And "I am on #{group1}'s page"
+  And "I follow \"Something\""
+  expect{
     And "I press \"Share\""
+  }.to change(ItemShare, :count).by(1)
 end
 
 Given /^"([^""]*)" is connected to "([^""]*)"$/ do |group, group2|
-    When "I am on #{group}'s page"
-      And "I follow \"Connect to Groups\""
-      And "I select \"#{group2}\" from \"group_id\""
-      And "I press \"Connect to Group\""
+  When "I am on #{group}'s page"
+  And "I follow \"Connect to Groups\""
+  And "I select \"#{group2}\" from \"group_id\""
+  expect {
+    And "I press \"Connect to Group\""
+  }.to change(Group.find_by_name(group).groups, :count).by(1)
 end
 
 Given /^"([^""]*)" has a Todo$/ do |group|
   When "I am on #{group}'s page"
-    And "I follow \"Add a Todo\""
-    And "I fill in \"Title\" with \"Something\""
+  And "I follow \"Add a Todo\""
+  And "I fill in \"Title\" with \"Something\""
+  expect{
     And "I press \"Create List\""
+  }.to change(Todo, :count).by(1)
 end
 
 Then /^I should see "([^""]*)" in the shared groups section$/ do |arg1|
