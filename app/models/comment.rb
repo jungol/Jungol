4b4a5ff6c5534b5da956ba685ad4425ef79f96a6
Ryland Herrick <ryland@ry-land.com>
2011-08-06 13:28:43 -0500

    Comments fixes.
    Removed character limit from comments.
    
    Fixed styling on comments.
    
    Discussion comments now styled same as todo comments.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,8 +5,7 @@ class Comment < ActiveRecord::Base
   belongs_to :item, :polymorphic => true
   belongs_to :user
 
-  validates( :body, :presence => true,
-                      :length => {:maximum => 512})
+  validates :body, :presence => true
 
   def item
     self.item
