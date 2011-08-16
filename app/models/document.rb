14f9a5be107c38cc0a326dbda693e3fb2fe1a457
Ryland Herrick <ryland@ry-land.com>
2011-08-16 13:55:52 -0500

    Fixed uploading of Office Docs.
    List of MIME types now more comprehensive.
    
    Some formatting of Download button.

--- app/models/document.rb
+++ app/models/document.rb
@@ -21,6 +21,31 @@ class Document < ActiveRecord::Base
   validates_attachment_presence :doc, :message => "must be present. You need to upload a file."
   validates_attachment_size :doc, :less_than => 4.megabyte, :message => "must be less than 4MB in size"
   validates_attachment_content_type :doc,
-    :content_type => /^(image\/(jpg|jpeg|pjpeg|png|x-png|gif))|(application\/(pdf|msword|vnd\.(ms\-(excel|powerpoint)|openxmlformats\-officedocument\.(wordprocessingml\.document|spreadsheetml\.sheet|presentationml\.(presentation|slideshow)))))$/,
+    :content_type => %w(image/jpg
+                        image/jpeg
+                        image/pjpeg
+                        image/png
+                        image/x-png
+                        image/gif
+                        application/pdf
+                        application/msword
+                        application/vnd.openxmlformats-officedocument.wordprocessingml.document
+                        application/vnd.openxmlformats-officedocument.wordprocessingml.template
+                        application/vnd.ms-word.document.macroEnabled.12
+                        application/vnd.ms-word.template.macroEnabled.12 application/vnd.ms-excel
+                        application/vnd.openxmlformats-officedocument.spreadsheetml.sheet
+                        application/vnd.openxmlformats-officedocument.spreadsheetml.template
+                        application/vnd.ms-excel.sheet.macroEnabled.12
+                        application/vnd.ms-excel.template.macroEnabled.12
+                        application/vnd.ms-excel.addin.macroEnabled.12
+                        application/vnd.ms-excel.sheet.binary.macroEnabled.12
+                        application/vnd.ms-powerpoint
+                        application/vnd.openxmlformats-officedocument.presentationml.presentation
+                        application/vnd.openxmlformats-officedocument.presentationml.template
+                        application/vnd.openxmlformats-officedocument.presentationml.slideshow
+                        application/vnd.ms-powerpoint.addin.macroEnabled.12
+                        application/vnd.ms-powerpoint.presentation.macroEnabled.12
+                        application/vnd.ms-powerpoint.template.macroEnabled.12
+                        application/vnd.ms-powerpoint.slideshow.macroEnabled.12),
     :message => 'is not allowed (only images, .pdfs, and MS Office filetypes)'
 end
