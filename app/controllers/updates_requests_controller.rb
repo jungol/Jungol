96d0ab87c9d45c77cb6a0498f76053cb20abac3b
Ryland Herrick <ryland@ry-land.com>
2011-08-24 17:05:09 -0500

    Added notification email for update requests

--- app/controllers/updates_requests_controller.rb
+++ app/controllers/updates_requests_controller.rb
@@ -5,6 +5,7 @@ class UpdatesRequestsController < ApplicationController
     req = UpdatesRequest.new(params[:updates_request])
     req.ip = @ip
     req.save!
+    Notifier.email_signup(req).deliver
       render :nothing => true
     rescue ActiveRecord::RecordInvalid => e
       render :text => e.message, :status => 403
